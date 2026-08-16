2da5494642b7a1d2a6683a5be2cf7e8a364ec637
briantrujillof <brian.trujillo.f@gmail.com>
2026-08-16 01:24:55 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,33 @@
-self.addEventListener('fetch', function(event) {});
+const CACHE_NAME = 'app-academica-v2';
+
+self.addEventListener('install', event => {
+    self.skipWaiting(); // Obliga al celular a usar esta nueva versión de inmediato
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(['/', '/index.html', '/style.css', '/app.js', '/manifest.json']);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    // Borra la memoria de la versión 1.0
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
